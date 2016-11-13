e8f7d70a14bb82b350418faf0aade50a9960da96
GsomGsom <GsomGsom@1baee301-0139-441a-b302-27d2aaaffe84>
2016-11-13 19:37:54 +0000

    init
    git-svn-id: https://subversion.assembla.com/svn/all-progs/MPI_Lab5@560 1baee301-0139-441a-b302-27d2aaaffe84

---
+++ MPI_Lab5/nbproject/Makefile-variables.mk
@@ -0,0 +1,59 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+CND_BUILDDIR=build
+CND_DISTDIR=dist
+# Debug configuration
+CND_PLATFORM_Debug=mpich2-Linux-x86
+CND_ARTIFACT_DIR_Debug=dist/Debug/mpich2-Linux-x86
+CND_ARTIFACT_NAME_Debug=mpi_lab5
+CND_ARTIFACT_PATH_Debug=dist/Debug/mpich2-Linux-x86/mpi_lab5
+CND_PACKAGE_DIR_Debug=dist/Debug/mpich2-Linux-x86/package
+CND_PACKAGE_NAME_Debug=mpilab5.tar
+CND_PACKAGE_PATH_Debug=dist/Debug/mpich2-Linux-x86/package/mpilab5.tar
+# Release configuration
+CND_PLATFORM_Release=GNU-Linux-x86
+CND_ARTIFACT_DIR_Release=dist/Release/GNU-Linux-x86
+CND_ARTIFACT_NAME_Release=mpi_lab5
+CND_ARTIFACT_PATH_Release=dist/Release/GNU-Linux-x86/mpi_lab5
+CND_PACKAGE_DIR_Release=dist/Release/GNU-Linux-x86/package
+CND_PACKAGE_NAME_Release=mpilab5.tar
+CND_PACKAGE_PATH_Release=dist/Release/GNU-Linux-x86/package/mpilab5.tar
+# Debug_10 configuration
+CND_PLATFORM_Debug_10=mpich2-Linux-x86
+CND_ARTIFACT_DIR_Debug_10=dist/Debug_10/mpich2-Linux-x86
+CND_ARTIFACT_NAME_Debug_10=mpi_lab5
+CND_ARTIFACT_PATH_Debug_10=dist/Debug_10/mpich2-Linux-x86/mpi_lab5
+CND_PACKAGE_DIR_Debug_10=dist/Debug_10/mpich2-Linux-x86/package
+CND_PACKAGE_NAME_Debug_10=mpilab5.tar
+CND_PACKAGE_PATH_Debug_10=dist/Debug_10/mpich2-Linux-x86/package/mpilab5.tar
+# Debug_3 configuration
+CND_PLATFORM_Debug_3=mpich2-Linux-x86
+CND_ARTIFACT_DIR_Debug_3=dist/Debug_3/mpich2-Linux-x86
+CND_ARTIFACT_NAME_Debug_3=mpi_lab5
+CND_ARTIFACT_PATH_Debug_3=dist/Debug_3/mpich2-Linux-x86/mpi_lab5
+CND_PACKAGE_DIR_Debug_3=dist/Debug_3/mpich2-Linux-x86/package
+CND_PACKAGE_NAME_Debug_3=mpilab5.tar
+CND_PACKAGE_PATH_Debug_3=dist/Debug_3/mpich2-Linux-x86/package/mpilab5.tar
+# Debug_100 configuration
+CND_PLATFORM_Debug_100=mpich2-Linux-x86
+CND_ARTIFACT_DIR_Debug_100=dist/Debug_100/mpich2-Linux-x86
+CND_ARTIFACT_NAME_Debug_100=mpi_lab5
+CND_ARTIFACT_PATH_Debug_100=dist/Debug_100/mpich2-Linux-x86/mpi_lab5
+CND_PACKAGE_DIR_Debug_100=dist/Debug_100/mpich2-Linux-x86/package
+CND_PACKAGE_NAME_Debug_100=mpilab5.tar
+CND_PACKAGE_PATH_Debug_100=dist/Debug_100/mpich2-Linux-x86/package/mpilab5.tar
+#
+# include compiler specific variables
+#
+# dmake command
+ROOT:sh = test -f nbproject/private/Makefile-variables.mk || \
+	(mkdir -p nbproject/private && touch nbproject/private/Makefile-variables.mk)
+#
+# gmake command
+.PHONY: $(shell test -f nbproject/private/Makefile-variables.mk || (mkdir -p nbproject/private && touch nbproject/private/Makefile-variables.mk))
+#
+include nbproject/private/Makefile-variables.mk
